c973ceabf77a956d3a3fb87860aac66668a3195c
developsomethingcool <pometunalexander@gmail.com>
2025-04-09 21:17:35 +0200

    Working solution of summarizer with local LLM model

--- youtube_summer.py
+++ youtube_summer.py
@@ -5,7 +5,9 @@ from ollama import ChatResponse
 def get_video_id(url_link):
   return url_link.split("watch?v=")[-1]
 
-video_id = get_video_id("https://www.youtube.com/watch?v=QTABGw29W5M&list=LL&index=1")
+video_link = "https://www.youtube.com/watch?v=wjZofJX0v4M"
+video_id = get_video_id(video_link)
+
 
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
@@ -13,7 +15,7 @@ transcript = YouTubeTranscriptApi.get_transcript(video_id)
 transcript_joined = " ".join([line['text'] for line in transcript])
 print(f'joined transcript {transcript_joined}')
 
-response = chat(model='llama3.1', messages=[
+response = chat(model='phi4:latest', messages=[
   {
     'role': 'user',
     'content': f"Summarize this lesson:\n\n{transcript_joined}"
