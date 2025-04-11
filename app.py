f2511da7f8a76e24de316526a60a71855b64f6bc
developsomethingcool <pometunalexander@gmail.com>
2025-04-11 18:27:08 +0200

    Improved UI

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from utils import get_video_id, get_transcript
 from summarizer import summarizer
+import re
 
 st.title("🎥 YouTube Video Summarizer")
 
@@ -10,11 +11,34 @@ url = st.text_input("Paste YouTube video link:")
 file_name = st.text_input("Paste the file name")
 
 if st.button("Summarize Video"):
-    video_id = get_video_id(url)
-    transcript = get_transcript(video_id=video_id)
-    summary = summarizer(transcript)
+    if not url:
+        st.error("Please provide Youtube URL.")
+    else:
+        with st.spinner("Fetching and summarizing video..."):
+            try:
+                video_id = get_video_id(url)
+                transcript = get_transcript(video_id=video_id)
 
-    st.markdown("### Summary")
-    st.write()
+                if not transcript:
+                    st.warning("Transcript not available for this video")
+                else:
+                    summary = summarizer(transcript)
+                    st.write(summary)
 
-    st.download_button("Download Summary", summary, file_name=file_name)
+                    # removed non allowed signs from the name
+                    safe_file_name = re.sub(r'[\\/*?:"<>|]', "_", file_name or "summary.txt")
+
+                    if not safe_file_name.endswith(".txt"):
+                        safe_file_name += ".txt"
+
+                    st.download_button(
+                        label="Download Summary",
+                        data=summary,
+                        file_name=safe_file_name,
+                        mime="text/plain"
+                    )
+            except Exception as e:
+                st.error(f"An error occured: {e}")
+    
+
+
